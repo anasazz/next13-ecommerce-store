0326a3bee045bc50b6ea74b7d39a099c663578f0
anas <anasazzouz8@gmail.com>
2023-09-19 21:35:50 +0100

    Update info.tsx

--- components/info.tsx
+++ components/info.tsx
@@ -18,6 +18,8 @@ const Info: React.FC<InfoProps> = ({ data }) => {
     cart.addItem(data);
   }
 
+
+  
   return ( 
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
